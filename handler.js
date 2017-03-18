360695bcbbc659846ca4a4c0f4cb5a673e7b1fbb
jodusnodus <billiet.thomas@outlook.com>
2017-03-18 15:00:48 +0100

    Initial commit

---
+++ handler.js
@@ -0,0 +1,20 @@
+if (!global._babelPolyfill) {
+  require("babel-polyfill");
+}
+
+const Handler = require("./lib/Handler");
+const MangaPanda = require("./lib/sources/MangaPanda");
+const MangaReader = require("./lib/sources/MangaReader");
+const MangaEden = require("./lib/sources/MangaEden");
+
+const handlers = [
+  new Handler("mangapanda", new MangaPanda()),
+  new Handler("mangareader", new MangaReader()),
+  new Handler("mangaeden", new MangaEden()),
+];
+
+module.exports = {};
+
+handlers.forEach((handler) => {
+  Object.assign(module.exports, handler.getPrefixedFuncs());
+});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: "./handler.js",
+  target: "node",
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel"],
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+};
